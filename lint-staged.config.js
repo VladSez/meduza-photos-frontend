264eed4a1578356dc09b8058c28f1f9f598dbeaa
VladSez <16666234+VladSez@users.noreply.github.com>
2023-07-23 21:52:12 +0200

    feat: add lint-stage

---
+++ lint-staged.config.js
@@ -0,0 +1,16 @@
+const path = require("path");
+
+module.exports = {
+  // Run type-check on changes to TypeScript files
+  // https://github.com/okonet/lint-staged#example-run-tsc-on-changes-to-typescript-files-but-do-not-pass-any-filename-arguments
+  "**/*.ts?(x)": () => "tsc -p tsconfig.json --noEmit",
+  // Run ESLint on changes to JavaScript/TypeScript files
+  "**/*.(ts)?(x)": (filenames) => [
+    `pnpm run lint ${filenames
+      .map((f) => path.relative(process.cwd(), f))
+      .join(" ")}`,
+    `pnpm run knip`,
+    `pnpm run prettify`,
+  ],
+  "**/*.(json|yaml)": () => "pnpm run knip",
+};
