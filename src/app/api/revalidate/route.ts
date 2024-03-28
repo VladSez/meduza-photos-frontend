de74eab282e219ef6b456c35544b2ac533163068
VladSez <16666234+VladSez@users.noreply.github.com>
2024-03-28 14:06:13 +0100

    feat: add on-demand revalidate endpoint

---
+++ src/app/api/revalidate/route.ts
@@ -0,0 +1,22 @@
+import { revalidatePath } from "next/cache";
+
+import { env } from "@/env.mjs";
+
+export function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+
+  const secret = searchParams.get("secret");
+
+  if (secret !== env.REVALIDATE_SECRET) {
+    return new Response(`Invalid credentials`, {
+      status: 500,
+    });
+  }
+
+  revalidatePath("/feed");
+  revalidatePath("/calendar");
+
+  return new Response("Revalidated", {
+    status: 200,
+  });
+}
