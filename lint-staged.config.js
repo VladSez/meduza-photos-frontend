711cd66ff234dd7593a7614e8562d52afac7320d
Vlad Sazonau <16666234+VladSez@users.noreply.github.com>
2024-04-02 17:13:51 +0200

    fix: styles + minor tweaks (#47)

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,11 +1,11 @@
 const path = require("path");
 
 module.exports = {
+  // Run ESLint on changes to JavaScript/TypeScript files
   // Run type-check on changes to TypeScript files
   // https://github.com/okonet/lint-staged#example-run-tsc-on-changes-to-typescript-files-but-do-not-pass-any-filename-arguments
-  "**/*.ts?(x)": () => "tsc -p tsconfig.json --noEmit",
-  // Run ESLint on changes to JavaScript/TypeScript files
-  "**/*.(ts)?(x)": (filenames) => [
+  "**/*.ts?(x)": (filenames) => [
+    `tsc -p tsconfig.json --noEmit`,
     `pnpm run lint ${filenames
       .map((f) => path.relative(process.cwd(), f))
       .join(" ")}`,
--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -7,7 +7,7 @@ import { CalendarListClient } from "./_components/calendar-client";
 
 import type { Metadata } from "next";
 
-export const revalidate = 43_200; // 12 hours
+export const revalidate = 3600; // 1 hour
 
 export const metadata: Metadata = {
   title: "Календарь",
