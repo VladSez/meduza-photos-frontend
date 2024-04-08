fa73abcc13012a8f8b09c55584228d48a18e9e67
Vlad Sazonau <16666234+VladSez@users.noreply.github.com>
2024-04-08 22:36:21 +0200

    feat: update revalidatePath call in route.ts to include /calendar (#58)

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -16,6 +16,7 @@ export function GET(request: Request) {
   // revalidate all pages
   // https://nextjs.org/docs/app/api-reference/functions/revalidatePath#revalidating-all-data
   revalidatePath("/", "layout");
+  revalidatePath("/calendar", "layout");
 
   return new Response("Revalidated successfully", {
     status: 200,
