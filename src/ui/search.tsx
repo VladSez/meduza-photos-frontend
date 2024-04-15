9b28902929d9c99bdbd9f74429bc58aa90308ee0
VladSez <16666234+VladSez@users.noreply.github.com>
2024-04-15 20:54:45 +0200

    feat: new animation for remove search query

--- src/ui/search.tsx
+++ src/ui/search.tsx
@@ -254,9 +254,9 @@ const SearchContent = ({ close }: SearchContentProps) => {
                               <motion.div
                                 key={item.id}
                                 layout
-                                initial={{ x: 0, opacity: 0 }}
-                                animate={{ x: 0, opacity: 1 }}
-                                exit={{ x: -300, opacity: 0 }}
+                                initial={{ scale: 0.8, opacity: 0 }}
+                                animate={{ scale: 1, opacity: 1 }}
+                                exit={{ scale: 0.8, opacity: 0 }}
                                 transition={{
                                   opacity: { duration: 0.2 },
                                 }}
