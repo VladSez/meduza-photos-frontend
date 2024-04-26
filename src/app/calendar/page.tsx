2ea5083cc33efa8598000b9073b7cf6cde0f9d2f
VladSez <16666234+VladSez@users.noreply.github.com>
2024-04-26 21:31:57 +0200

    feat: add basic css animations

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -14,7 +14,7 @@ export default async function Calendar() {
   const { posts } = await fetchPosts({ take: 10 });
 
   return (
-    <article>
+    <article className="animate-in fade-in">
       <h1 className="mx-auto mb-10 mt-24 max-w-4xl text-center text-4xl font-extrabold tracking-tight text-gray-900 dark:text-white sm:text-5xl md:mt-32 lg:text-6xl">
         Хроники войны в Украине
       </h1>
