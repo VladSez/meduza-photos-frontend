847d9cdf291c35cae2f670779d605a4e9639df76
VladSez <16666234+VladSez@users.noreply.github.com>
2024-06-10 00:18:21 +0200

    fix: edge error

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -13,5 +13,6 @@ Sentry.init({
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
-  enabled: process.env.NODE_ENV !== "development",
+  // enabled: process.env.NODE_ENV !== "development",
+  enabled: false, // Disable Sentry for now, try to fix edge runtime error
 });
